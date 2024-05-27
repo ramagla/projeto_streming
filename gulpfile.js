9d6a6033ca4aace438bb8efafd645feeab9dba42
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-05-27 08:35:31 -0300

    corrigindo imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,41 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin')
-const uglify = require('gulp-uglify')
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
-
-function scripts() {  
-    return gulp.src('./src/scripts/*.js')
-   .pipe(uglify())
-   .pipe(gulp.dest('./dist/js')) 
+function scripts() {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
 }
 
-
-function styles(){
-    return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+function styles() {
+  return gulp
+    .src("./src/styles/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("./dist/css"));
 }
 
-function images(){
-    return gulp.src('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'));
-}
+// function images(){
+//     return gulp.src('./src/images/**/*')
+//     .pipe(imagemin())
+//     .pipe(gulp.dest('./dist/images'));
+// }
 
+function images() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(
+      imagemin([
+        imagemin.mozjpeg({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+      ])
+    )
+    .pipe(gulp.dest("./dist/images"));
+}
 exports.default = gulp.parallel(styles, images, scripts);
-exports.watch = function() {
-    gulp.watch('./src/styles/*scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*js', gulp.parallel(sripts))
-}
+exports.watch = function () {
+  gulp.watch("./src/styles/*scss", gulp.parallel(styles));
+  gulp.watch("./src/scripts/*js", gulp.parallel(sripts));
+};
